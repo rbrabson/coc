c0ac4f3d73bef142c902ea04a9e0fc97dc03ba41
Roy Brabson <rbrabson@gmail.com>
2022-03-21 17:01:47 -0400

    initial commit

--- v1/cwl.go
+++ v1/cwl.go
@@ -18,28 +18,55 @@ func (wl ClanWarLeague) String() string {
 
 // ClanWarLeagueGroup is a clan's current clan war league group.
 type ClanWarLeagueGroup struct {
-	Clans []struct {
-		BadgeUrls BadgeUrls `json:"badgeUrls"`
-		ClanLevel int       `json:"clanLevel"`
-		Members   []struct {
-			Name          string `json:"name"`
-			Tag           string `json:"tag"`
-			TownHallLevel int    `json:"townHallLevel"`
-		} `json:"members"`
-		Name string `json:"name"`
-		Tag  string `json:"tag"`
-	} `json:"clans"`
-	Rounds []struct {
-		WarTags []string `json:"warTags"`
-	} `json:"rounds"`
-	Season string `json:"season"`
-	State  string `json:"state"`
-	Tag    string `json:"tag"`
+	Clans  []ClanWarLeagueClan  `json:"clans"`
+	Rounds []ClanWarLeagueRound `json:"rounds"`
+	Season string               `json:"season"`
+	State  string               `json:"state"`
+	Tag    string               `json:"tag"`
 }
 
-// String returns a string representation of a clan war league group
+// String returns a string representation of the group of clans in a clan war league group
 func (lg ClanWarLeagueGroup) String() string {
 	b, _ := json.Marshal(lg)
+	return string(b)
+}
+
+// ClanWarLeagueClan is a clan in a clan war league group.
+type ClanWarLeagueClan struct {
+	BadgeUrls BadgeUrls             `json:"badgeUrls"`
+	ClanLevel int                   `json:"clanLevel"`
+	Members   []ClanWarLeagueMember `json:"members"`
+	Name      string                `json:"name"`
+	Tag       string                `json:"tag"`
+}
+
+// String returns a string representation of the clan in a clan war league group
+func (lg ClanWarLeagueClan) String() string {
+	b, _ := json.Marshal(lg)
+	return string(b)
+}
+
+// ClanWarLeagueRound is a round in a clan war league group.
+type ClanWarLeagueRound struct {
+	WarTags []string `json:"warTags"`
+}
+
+// String returns a string representation of a clan in a clan war league group.
+func (lr ClanWarLeagueRound) String() string {
+	b, _ := json.Marshal(lr)
+	return string(b)
+}
+
+// ClanWarLeagueMember is a member of a clan in a clan war league group.
+type ClanWarLeagueMember struct {
+	Name          string `json:"name"`
+	Tag           string `json:"tag"`
+	TownHallLevel int    `json:"townHallLevel"`
+}
+
+// String returns a string representation of a member in a clan in a clan war league group.
+func (cm ClanWarLeagueMember) String() string {
+	b, _ := json.Marshal(cm)
 	return string(b)
 }
 
